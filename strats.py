1c05b923589f6b4d441b26d62d99df9d6c8e2349
Aditya Gupta <84505010+RockstarBuddies@users.noreply.github.com>
2024-05-08 00:08:08 +0530

    Update strats.py

--- strats.py
+++ strats.py
@@ -15,5 +15,18 @@ def generoust4t(enemyMoves):
 def fiftyfifty(enemyMoves):
     return random.choice([0,1])
 
-strategies = [t4t, generoust4t, fiftyfifty]
+def inverse_t4t(enemyMoves):
+    if not enemyMoves:
+        return 1
+    elif enemyMoves[-1]==0:
+        return 1
+    return 0
+
+def angel(enemyMoves):
+    return 0
+
+def devil(enemyMoves):
+    return 1
+
+strategies = [t4t, generoust4t, fiftyfifty, inverse_t4t, angel, devil]
 
